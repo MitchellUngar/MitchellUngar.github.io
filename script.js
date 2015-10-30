72c7570619c85ed18f83159305772e2b4d704e18
Mitchell.Ungar <mitchell.ungar@bbd.com>
2015-10-30 12:24:47 -0400

    Initial Commit

---
+++ script.js
@@ -0,0 +1,91 @@
+/*
+TITLE: Date and Time Javascript Windows.onload
+AUTHOR: Mitchell Ungar
+DATE: October 16 2015
+
+What this javascript does is allow the webpage display a Date and time that always is running.
+*/
+/*Beginning of Date function*/
+function initDate() {
+  /*an Array of months and days*/
+  var dayName = new Array("Sunday", "Monday","Tuesday","Wednesday","Thursday","Friday","Saturday");
+  var monName = new Array("January", "February", "March", "April", "May", "June", "July", "August",
+   "September", "October","November","December");
+
+   /*A variable being used to get the current date*/
+   var now = new Date();
+   /*Gets the months index, but isn't starting at 0 like Array, so +1 to make it correct month */
+   var thisMonth = now.getMonth() + 1;
+   /*The display string that is being sent to the index.html*/
+   var dtString = monName[now.getMonth()] + " " + now.getDate() + ", " + now.getFullYear();
+   /*Sending information to the HTML's <h1> tag called dtField*/
+   document.getElementById("dtField").innerHTML = dtString;
+}
+/*End of Date Function*/
+/*Beginning of showTheTime function*/
+function showTheTime() {
+  /*Creating a current date variable*/
+  var now = new Date();
+  /*String that is being sent to the HTML page*/
+  var theTime = showTheHours(now.getHours()) +
+  showZeroFilled(now.getMinutes()) + showZeroFilled(now.getSeconds())
+  + showAmPm();
+  /*Sending the String tot he HTML page*/
+  document.getElementById("showTime").innerHTML = theTime;
+  /*start the showTheTime function ever 1 second increment time on display*/
+  setTimeout("showTheTime()", 1000);
+  /*Javascript runs on a 24 hour clock, this displays 1-12 for the HTML*/
+  function showTheHours(theHour) {
+    if(theHour == 0) {
+        return 12;
+      }
+      if (theHour < 13) {
+        return theHour;
+      }
+      return theHour-12;
+    }
+  /*anything before 10 seconds gets a 0 before it*/
+  function showZeroFilled(inValue) {
+    if (inValue > 9) {
+      return ":" + inValue;
+    }
+    return ":0" + inValue;
+  }
+  /*puts AM or PM at the end of the time*/
+  function showAmPm() {
+    if(now.getHours() < 12) {
+      return " am";
+    }
+    return " pm";
+  }
+}
+/*End of show the time function*/
+
+/*Javascript isn't smart enough to have two window.onload functions being used
+So I created a function the runs the other two functions so all I have to do
+is call windows.onload once*/
+function start() {
+  initDate();
+  showTheTime();
+}
+var video_out = document.getElementById("vid-box");
+function login(form) {
+    var phone = window.phone = PHONE({
+        number        : form.username.value || "Anonymous", // listen on username line else Anonymous
+        publish_key   : 'pub-c-c0eba782-00c1-497f-afa4-d766b32d66af',
+        subscribe_key : 'sub-c-cf519308-79b5-11e5-9720-0619f8945a4f',
+    });
+    phone.ready(function(){ form.username.style.background="#55ff5b"; });
+    phone.receive(function(session){
+        session.connected(function(session) { video_out.appendChild(session.video); });
+        session.ended(function(session) { video_out.innerHTML=''; });
+    });
+    return false;   // So the form does not submit.
+}
+function makeCall(form){
+    if (!window.phone) alert("Login First!");
+    else phone.dial(form.number.value);
+    return false;
+}
+/*Starts the date and time functions at the same time*/
+window.onload = start;
