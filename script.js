f0c9b33d5f61634537e2860e66f61dacfeca19e5
Mitchell.Ungar <mitchell.ungar@bbd.com>
2015-11-06 11:14:53 -0500

    Adding calender function

--- script.js
+++ script.js
@@ -59,7 +59,26 @@ function showTheTime() {
     return " pm";
   }
 }
+/*  jQuery ready function. Specify a function to execute when the DOM is fully loaded.  */
+$(document).ready(
 
+  /* This is the function that will get executed after the DOM is fully loaded */
+  function () {
+    $( "#datepicker" ).datepicker({
+      changeMonth: true,//this option for allowing user to select month
+      changeYear: true //this option for allowing user to select from year range
+    });
+    $( "#datepicker2" ).datepicker({
+      changeMonth: true,//this option for allowing user to select month
+      changeYear: true //this option for allowing user to select from year range
+    });
+    $( "#datepicker3" ).datepicker({
+      changeMonth: true,//this option for allowing user to select month
+      changeYear: true //this option for allowing user to select from year range
+    });
+  }
+
+);
 /*End of show the time function*/
 
 /*Javascript isn't smart enough to have two window.onload functions being used
