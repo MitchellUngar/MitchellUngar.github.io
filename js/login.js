c3026e0e712e045efc3cfe6b9ec5539e50e3e82b
Mitchell.Ungar <mitchell.ungar@bbd.com>
2015-11-26 13:47:34 -0500

    Adding example videos

--- js/login.js
+++ js/login.js
@@ -9,14 +9,34 @@ function login() {
   var coruser = "Admin";
   var corpass = "Mitchell";
 
-  var coruser2 = "User";
-  var corpass2 = "Jessica";
+  var coruser2 = "Jessica";
+  var corpass2 = "BJessica";
+
+  var coruser3 = "Julie";
+  var corpass3 = "EJulie";
 
   if(user.value == coruser) {
     if(pass.value == corpass){
       window.alert("You are logged in as " + user.value);
-      window.open("editbuzzboardindex.html");
       tricky_win_close();
+      window.open("editbuzzboardindex.html");
+    }
+  }
+
+  if(user.value == coruser3) {
+    if(pass.value == corpass3){
+      window.alert("You are logged in as " + user.value);
+      tricky_win_close();
+      window.open("editbuzzboardindex.html");
+    }
+  }
+
+  if(user.value == coruser2) {
+    if(pass.value == corpass2){
+      window.alert("You are logged in as " + user.value);
+      tricky_win_close();
+      window.open("editbuzzboardindex.html");
+
     } else {
       window.alert("Incorrect username or password");
     }
